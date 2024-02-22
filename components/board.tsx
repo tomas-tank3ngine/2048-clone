fe1939818d72dbc8db53fd8200ad63054cb0acb0
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-02-22 05:44:20 -0500

    Add tiles and tiles container

--- components/board.tsx
+++ components/board.tsx
@@ -1,4 +1,5 @@
 import styles from "@/styles/board.module.css";
+import Tile from "./tile";
 
 export default function Board() {
   const renderGrid = () => {
@@ -14,6 +15,9 @@ export default function Board() {
 
   return (
     <div className={styles.board}>
+      <div className={styles.tiles}>
+        <Tile />
+      </div>
       <div className={styles.grid}>{renderGrid()}</div>
     </div>
   );
