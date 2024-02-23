ea6e68f2805d3d46b7ea3c93a867523c369d5418
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-02-23 18:56:20 -0500

    add scale animations when merging

--- hooks/use-previous-props.ts
+++ hooks/use-previous-props.ts
@@ -1,3 +1,11 @@
-export default function usePreviousProps<K = any>(value: K){
+import { useEffect, useRef } from "react"
 
+export default function usePreviousProps<K = any>(value: K){
+  const ref = useRef<K>()
+
+  useEffect(() => {
+    ref.current = value;
+  })
+
+  return ref.current;
 }
