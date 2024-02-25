2e6dfd3500ff6d90e42fc9911fc0df434d2523d3
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-02-24 22:45:54 -0500

    add responsive styles and js

--- components/tile.tsx
+++ components/tile.tsx
@@ -1,14 +1,18 @@
 import {
-  containerWidth,
   tileCountPerDimension,
   mergeAnimationDuration,
+  containerWidthDesktop,
+  containerWidthMobile,
 } from "@/constants";
 import usePreviousProps from "@/hooks/use-previous-props";
 import { Tile as TileProps } from "@/models/tile";
 import styles from "@/styles/tile.module.css";
 import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function Tile({ position, value }: TileProps) {
+  const isWideScreen = useMediaQuery({minWidth: 512})
+  const containerWidth = isWideScreen ? containerWidthDesktop : containerWidthMobile;
   const [scale, setScale] = useState(1);
   const previousValue = usePreviousProps(value); //auto detects the type (value: number)
   const hasChanged = previousValue != value;
--- constants.ts
+++ constants.ts
@@ -1,6 +1,8 @@
 export const tileCountPerDimension = 4;
 
-export const containerWidth = 288; //pixels
+export const containerWidthMobile = 288; //pixels
+
+export const containerWidthDesktop = 464; //pixels
 
 export const mergeAnimationDuration = 100; //ms
 
